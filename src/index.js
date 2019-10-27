48bc649789f89216b72872f927b2bf1f15aabd97
higer <higer@gmail.com>
2019-10-27 11:30:20 +1100

    add resource management

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import _ from "lodash";
+import "./style.css";
+import Icon from "./icon.png";
 function component() {
   var element = document.createElement("div");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
+
+  //add png to the current div
+  var myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
 
   return element;
 }
